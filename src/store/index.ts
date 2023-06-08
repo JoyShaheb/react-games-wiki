6971645e8f57ec74da38e2902670ed6e51aeb6ac
joyshaheb <khondokoralam@gmail.com>
2023-06-09 02:09:46 +0600

    added redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,43 @@ import { resetSystem, sysmtemSlice, themeSwitch } from "./Slices/System";
 import { GamesAPI, useGetAllGamesQuery } from "./API/GamesAPI";
 import { useGetAllPlatformsQuery, PlatformAPI } from "./API/PlatformAPI";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedSystemReducer = persistReducer(
+  persistConfig,
+  sysmtemSlice.reducer
+);
+
 export const store = configureStore({
   reducer: {
     [GamesAPI.reducerPath]: GamesAPI.reducer,
     [PlatformAPI.reducerPath]: PlatformAPI.reducer,
-    system: sysmtemSlice.reducer,
+    system: persistedSystemReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(GamesAPI.middleware, PlatformAPI.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(GamesAPI.middleware, PlatformAPI.middleware),
 });
 
+export const persistedStore = persistStore(store);
 setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
